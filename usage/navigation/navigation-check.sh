75cc76da5d2cf69f7d090883e5e2aea0bb1b58a5
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-06-12 12:00:42 +0200

    hide error message

--- usage/navigation/navigation-check.sh
+++ usage/navigation/navigation-check.sh
@@ -14,7 +14,7 @@ EOF
 sync
 ###################
 
-if ! cmp /tmp/.tree.got /tmp/.tree.expect ; then
+if ! cmp /tmp/.tree.got /tmp/.tree.expect 2>/dev/null; then
     diff -y /tmp/.tree.got /tmp/.tree.expect
     echo "Erreur: on dirait que vous n'avez la bonne arborescence (a gauche: ce que vous avez; a droite: ce qu'il faut)"
     exit 1
