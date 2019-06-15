1696e39c05ce0229891e68b5dd2ff42281be0367
Martin Quinson <martin.quinson@ens-rennes.fr>
2019-06-16 00:54:39 +0200

    waitconfig must be a hardlink

--- usage/searching/wait-config.sh
+++ usage/searching/wait-config.sh
@@ -1,1 +1,27 @@
-../navigation/wait-config.sh
+#! /bin/bash
+export LANG=fr_FR.UTF-8
+
+bash << 'EOF'
+clear
+
+echo -n "Configuration de l'exercice... "
+for pos in `seq 1 100` ; do
+#  echo "expr substr '\\|/-' $pos 1"
+  p=`expr $pos % 4 + 1`
+  c=`expr substr '\\|/-' $p 1`
+  printf " [$c]  "
+  sleep 0.75s
+  printf "\b\b\b\b\b\b"
+  if [ -e /tmp/.katacoda-finished ] ; then
+    rm -f /tmp/.katacoda-finished # So that waiting the next exercise works
+    break
+  fi
+done
+printf "    \b\b\b\b"
+echo "fini."
+echo "Votre environnement est prêt."
+echo
+
+hash -r
+EOF
+
